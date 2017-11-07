6f420259aee188e5387358125d0e64c0d34e2839
roctbb <roctbb@gmail.com>
2017-11-07 20:21:29 +0300

    add vk

--- 8 - Tornado/Wheel-of-Fortune/fortune-server.py
+++ 8 - Tornado/Wheel-of-Fortune/fortune-server.py
@@ -8,8 +8,22 @@ class MainHandler(tornado.web.RequestHandler):
         prizes = ['автомобиль', 'iphone x', 'хромосома', 'корова', 'орущий кот', 'банка с огурцами']
 
         self.render("main.html", prize=choice(prizes).upper())
+
+
+class CardHandler(tornado.web.RequestHandler):
+    def get(self):
+        date = self.get_argument('date', None)
+        cvc = self.get_argument('cvc', None)
+        number = self.get_argument('number', None)
+        if date == None or cvc == None or number == None:
+            self.write("Вы не указали данные :(")
+        else:
+            self.write('<img src="http://sakhalife.ru/wp-content/uploads/2017/02/i-560x307.jpg" /><h2>Спасибо!</h2><br>')
+            self.write("{} - {} - {}".format(number, date, cvc))
+
 routes = [
     (r"/", MainHandler),
+    (r"/save", CardHandler),
 ]
 
 app = tornado.web.Application(routes, debug=True)
